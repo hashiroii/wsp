df76c733bb4638db2ae5a0065a99dc48fb0df657
magzhan <mubarakulymagzhan@gmail.com>
2026-07-10 06:46:43 +0500

    fix(app): use AppCompatActivity and AppCompat theme for locale switching
    AppCompatDelegate.setApplicationLocales() requires AppCompatActivity for
    its delegate lifecycle hooks. ComponentActivity silently stores the locale
    but never triggers recreation. Theme.AppCompat.DayNight.NoActionBar
    satisfies the AppCompatDelegateImpl contract; all visual theming is handled
    by WspTheme inside setContent.

--- composeApp/src/androidMain/kotlin/kz/kbtu/wsp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/kz/kbtu/wsp/MainActivity.kt
@@ -1,11 +1,11 @@
 package kz.kbtu.wsp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- composeApp/src/androidMain/kotlin/kz/kbtu/wsp/MainApplication.kt
+++ composeApp/src/androidMain/kotlin/kz/kbtu/wsp/MainApplication.kt
@@ -5,8 +5,15 @@ import kz.kbtu.wsp.di.initKoin
 import org.koin.android.ext.koin.androidContext
 
 class MainApplication : Application() {
+
+    companion object {
+        lateinit var instance: MainApplication
+            private set
+    }
+
     override fun onCreate() {
         super.onCreate()
+        instance = this
         initKoin {
             androidContext(this@MainApplication)
         }
