187853846e0197350ed6c1213fcf38b301e79d54
magzhan <mubarakulymagzhan@gmail.com>
2026-07-18 23:25:46 +0500

    refactor(profile): remove dead isLoading field from ProfileState
    Nothing ever toggled it since profile data is currently synchronous
    mock data with no async load to back it.

--- feature/profile/src/commonMain/kotlin/kz/kbtu/wsp/feature/profile/ProfileState.kt
+++ feature/profile/src/commonMain/kotlin/kz/kbtu/wsp/feature/profile/ProfileState.kt
@@ -2,7 +2,6 @@ package kz.kbtu.wsp.feature.profile
 
 data class ProfileState(
     val profile: ProfileData = ProfileData(),
-    val isLoading: Boolean = false,
     val showPhotoPreview: Boolean = false,
     val activeSection: ProfileSection = ProfileSection.MainInfo
 )
